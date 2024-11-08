985c6f0f9c80e6106246424290ee878486676bd2
Gustavo Ignacio <gustavoignacio280@gmail.com>
2024-11-08 16:52:23 -0300

    Pequenos ajustes de lógica e novos testes de layout

--- app/src/main/java/br/gustavoIgnacio/easypetvet/MainActivity.java
+++ app/src/main/java/br/gustavoIgnacio/easypetvet/MainActivity.java
@@ -1,24 +1,99 @@
 package br.gustavoIgnacio.easypetvet;
 
-import android.os.Bundle;
+/*
+@author: <Gustavo da Silva Ignacio 1110482313006>
+*/
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Carrega o fragmento baseado nos dados do Bundle
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            carregaFragment(bundle);
+        }
+    }
+
+    private void carregaFragment(Bundle bundle) {
+        String tipoItem = bundle.getString("tipoItem");
+        if (tipoItem != null) {
+            if (tipoItem.equals("Animais")) {
+                fragment = new FragmentAnimais();
+            } else if (tipoItem.equals("Vacinas")) {
+                fragment = new FragmentVacinas();
+            } else if (tipoItem.equals("Consultas")) {
+		fragment = new FragmentConsultas();
+ 	    } 
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment, fragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        Bundle bundle = new Bundle();
+        Intent intent = new Intent(this, MainActivity.class);
+
+        if (id == R.id.menu_vacinas) {
+            bundle.putString("tipoItem", "Vacinas");
+
+            intent.putExtras(bundle);
+            this.startActivity(intent);
+            this.finish();
+            return true;
+        }
+        if (id == R.id.menu_animais) {
+            bundle.putString("tipoItem", "Animais");
+
+            intent.putExtras(bundle);
+            this.startActivity(intent);
+            this.finish();
+            return true;
+        }
+        if (id == R.id.menu_consultas) {
+            bundle.putString("tipoItem", "Consultas");
+
+            intent.putExtras(bundle);
+            this.startActivity(intent);
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
